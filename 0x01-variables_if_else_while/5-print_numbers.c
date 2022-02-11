c99336b2daa17350af55b5c9153a75600064a43e
shootskull <alex.cerong@hotmail.com>
2022-02-11 14:46:14 -0800

    .

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
+
 /**
- * main - prints the alphabet in lowercase, and then in uppercase,
- * followed by a new line
+ * main - prints all single digit numbers of base
+ * 10 starting from 0, followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int = a
-for (a = 0 ;  a < 10 ; a++)
-{
-printf("%d", a);
-printf('\n')
-}
-return (0);
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
 }
