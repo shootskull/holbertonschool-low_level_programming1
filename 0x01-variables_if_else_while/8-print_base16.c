7edaeed5e988b2d7d0453ddfe9c0ae02a0becf21
shootskull <alex.cerong@hotmail.com>
2022-02-11 15:37:51 -0800

    .

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,21 @@
-aaaaa
+#include <stdio.h>
+/**
+ * main -hola mundo
+ * a
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+char a;
+for (n = 48; n <= 57; n++)
+{
+putchar(n);
+}
+for (a = 97; a <= 102; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
