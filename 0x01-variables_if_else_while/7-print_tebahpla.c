38b6f265863e810ad2ad63cb727f2b888f6e1123
shootskull <alex.cerong@hotmail.com>
2022-02-11 15:29:01 -0800

    .

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,16 @@
-aaaaaa
+#include <stdio.h>
+/**
+* main -hola mundo
+* a
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n;
+for (n = 'z'; n >= 'a'; n--)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
