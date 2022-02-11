8f93220e0c899d9af86b3ecb3abae00a636b5718
shootskull <alex.cerong@hotmail.com>
2022-02-11 13:58:41 -0800

    .

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 /**
- * main - return the n value
+ * main - return
  * Return: return 0
  */
 int main(void)
 {
 int ch;
-for (ch = 'a'; ch <= 'z'; ch++)
+for (ch = 'a'; ch <= 'z'; ch++){
 putchar(ch);
-for (ch = 'A'; ch <= 'Z'; ch++)
-	putchar(ch);
+}
+for (ch = 'A'; ch <= 'Z'; ch++){
+putchar(ch);
+}
 putchar('\n');
-return 0;
+return (0) ;
+}
