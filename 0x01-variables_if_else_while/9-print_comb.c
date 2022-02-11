bc91fe042fae2b44d6ac6ff87223c663123ba330
shootskull <alex.cerong@hotmail.com>
2022-02-11 15:55:48 -0800

    .

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,21 @@
-aaaaa
+#include <stdio.h>
+/**
+*a
+* a
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int = n
+for (n=48 ; n<=57 ; n++)
+{
+putchar(n);
+if (n != 57)
+{
+putchar(', ');
+putchar(' ');
+}
+}
+putchar(\n);
+return (0);
+}
