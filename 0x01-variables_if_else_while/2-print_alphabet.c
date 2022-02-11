3a533879b690e25b2c8db01303d5fee8287595d0
shootskull <alex.cerong@hotmail.com>
2022-02-11 13:10:28 -0800

    .

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /*
- *
- *
- */
+*
+*
+*/
 int main ()
 {
-	char ch;
-	for(ch = 'a' ; ch <= 'z' ; ch++) {
-		putchar(ch);
-	}
-	return(0);
+char ch;
+for(ch = 'a' ; ch <= 'z' ; ch++) {
+putchar(ch);
+}
+return(0);
 }
