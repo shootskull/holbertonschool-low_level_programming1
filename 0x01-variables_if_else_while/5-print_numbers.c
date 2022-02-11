54b683cd3221dcdb53aaac52f132aa8e42397189
shootskull <alex.cerong@hotmail.com>
2022-02-11 14:37:01 -0800

    .

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,16 @@
-aaaa
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase, and then in uppercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int = a
+for (a = 0 ;  a < 10 ; a++)
+{
+printf("%d", a);
+printf('\n')
+}
+return (0);
+}
