f474d22a411f0396b2a14caad1c4d4c99cef3aaf
shootskull <alex.cerong@hotmail.com>
2022-02-11 14:12:28 -0800

    .

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,16 @@
-aaaa
+#include <stdio.h>
+/**
+ * main - return the n value
+ * Return: return 0
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if(ch != 'e' && !='q');
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
