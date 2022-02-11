fcfc60f3d90d751db4150aec9ab39a6806af38b3
shootskull <alex.cerong@hotmail.com>
2022-02-11 12:12:17 -0800

    .

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,11 +10,11 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if ( n% 10 >5)
-printf("%d and is greater than 5 \n")
+  printf("%d and is greater than 5 \n");
 }else if (n % 10 ==0){
-printf("%d and is 0 \n")
+  printf("%d and is 0 \n");
 }else (n % 10 <6 && n % 10 !=0){
-printf("%d and is less than 6 and not 0 \n")
+  printf("%d and is less than 6 and not 0 \n");
 }
 return (0);
 }
