c1b14b00c43855c435ef2ba92894ae4675f9dab0
shootskull <alex.cerong@hotmail.com>
2022-02-11 12:07:41 -0800

    .

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,1 +1,19 @@
-a
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if ( n% 10 >5)
+printf("%d and is greater than 5 \n")
+}else if (n % 10 ==0){
+printf("%d and is 0 \n")
+}else (n % 10 <6 && n % 10 !=0){
+printf("%d and is less than 6 and not 0 \n")
+}
+return (0);
+}
