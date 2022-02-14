9fe6dc0c3c841d0877cc232d399534b25b2ddf72
shootskull <alex.cerong@hotmail.com>
2022-02-14 06:41:49 -0800

    .

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,18 @@
-aaa
+#include <stdio.h>
+/**
+* main - return
+* Return: return 0
+*/
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
