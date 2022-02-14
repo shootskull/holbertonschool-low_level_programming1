398a312aa0a6cacad2fa1f970378389d52efb5b8
shootskull <alex.cerong@hotmail.com>
2022-02-14 07:10:53 -0800

    .

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,18 @@
-aaa
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet (void)
+{
+	char ch;
+	ch = 'a';
+	while ( ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+}
