b3672c83f3fb7f2f822e8b47c44481ecd8610840
shootskull <alex.cerong@hotmail.com>
2022-02-15 07:45:10 -0800

    .

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,18 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * main - check the code
+ * print_alphabet - check the code
  *
  * Return: Always 0.
  */
 void print_alphabet(void)
 {
-	char ch;
-	ch = 'a';
-	while (ch <= 'z')
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
 	{
-		_putchar(ch);
-		ch++;
+		_putchar(c);
+		c++;
 	}
 	_putchar('\n');
 }
