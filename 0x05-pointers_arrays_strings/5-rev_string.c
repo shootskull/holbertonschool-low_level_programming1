1f29792dea0e009b4126521d8db2ef4bf0fb323a
shootskull <alex.cerong@hotmail.com>
2022-02-21 15:08:41 -0800

    .

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,1 +1,23 @@
-a
+#include "main.h"
+/**
+ * rev_string - reserve a string
+ * @s: string to be printed
+ */
+
+void rev_string(char *s)
+{
+	char str[100];
+	int i = 0;
+	int j = 0;
+
+	while (s[i])
+	{
+		str[i] = s[i];
+		i++;
+	}
+	for (i = i - 1; i >= 0; i--)
+	{
+		s[i] = str[j];
+		j++;
+	}
+}
